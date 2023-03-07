23a0cd46879394a409573d507027a97b50b66129
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-03-07 23:47:06 +0200

    Created getAllProduct attached to @Get in product.controller

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,12 +3,16 @@ import { Product } from './product.model';
 
 @Injectable()
 export class ProductsService {
-  products: Product[] = [];
+  private products: Product[] = [];
 
   insertProduct(title: string, desc: string, price: number) {
     const prodId = Math.floor(Math.random() * 100000).toString();
     const newProduct = new Product(prodId, title, desc, price);
     this.products.push(newProduct);
     return prodId;
+  }
+
+  getProducts() {
+    return [...this.products];
   }
 }
