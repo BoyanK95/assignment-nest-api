ca51f4e10c1f7be9f9bab2b3fb55f4667c38e708
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-04-10 09:18:22 +0300

    Installed cors for fixing cors req constrain

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as cors from 'cors'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cors())
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
 }
